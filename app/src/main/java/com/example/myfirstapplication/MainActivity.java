e56f812bba3ec129aeeeb0489f5a7c61a98a19a0
SamMacbokAir <w304714766@gmail.com>
2023-08-11 13:58:14 +1000

    Day1

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view) {
+        TextView txtHello = findViewById(R.id.textView);
+        txtHello.setText("Hello Zhiyuan");
+    }
+
+    public void add(View view) {
+        EditText et1 = findViewById(R.id.editTextNumber);
+        EditText et2 = findViewById(R.id.editTextNumber2);
+
+        int firstNum = Integer.parseInt(et1.getText().toString());
+        int secondNUm = Integer.parseInt(et2.getText().toString());
+
+        TextView txtResult = findViewById((R.id.result));
+        int res = firstNum + secondNUm;
+        txtResult.setText("This is: " + res);
+    }
+
+    public void nextPage(View view) {
+        startActivity(new Intent(this, MainActivity2.class));
     }
 }
---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void goBack(View view) {
+        Intent returnIntent = new Intent();
+        setResult(RESULT_OK, returnIntent);
+
+        super.onBackPressed();
+    }
+}
